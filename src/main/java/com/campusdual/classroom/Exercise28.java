043e853521bbeb005e097c04153fdfdc06803965
BraisVarela <braisvarela6@gmail.com>
2024-11-07 17:03:05 +0100

    Ejercicio 28 Hecho

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,19 @@
 package com.campusdual.classroom;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exercise28 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+    launchException();
 
     }
+
+    public static void launchException() throws Exception {
+        throw new Exception("This is a test exception");
+    }
+
 }
